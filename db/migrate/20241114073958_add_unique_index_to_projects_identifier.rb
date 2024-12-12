0336c821d2565b9310b12ac848510dffb03e81df
HiroyukiKano <kano@netlab.jp>
2024-12-12 16:48:18 +0900

    changeメソッドに切り替え

--- db/migrate/20241114073958_add_unique_index_to_projects_identifier.rb
+++ db/migrate/20241114073958_add_unique_index_to_projects_identifier.rb
@@ -1,9 +1,5 @@
 class AddUniqueIndexToProjectsIdentifier < ActiveRecord::Migration[7.2]
-  def up
+  def change
     add_index :projects, :identifier, :unique => true
-  end
-
-  def down
-    remove_index :projects, :identifier
   end
 end
